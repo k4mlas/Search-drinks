a1ab9d5b5a8d7f7315781ac9bc2e2158c4e70526
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-04-01 17:17:14 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
 const input = document.querySelector('.card__header__search');
-const list = document.querySelectorAll('li');
+const lists = document.querySelectorAll('li');
 
 const checkList = () => {
 	const text = input.value.toLowerCase();
 
-	list.forEach((drink) => {
-		if (drink.textContent.toLowerCase().indexOf(text) !== -1) {
-			drink.style.display = 'block';
+								// Sposób z pętlą for of - zmienna list of lists
+
+	for (const list of lists) {
+		if (list.textContent.toLowerCase().indexOf(text) !== -1) {
+			list.style.display = 'block';
 		} else {
-			drink.style.display = 'none';
+			list.style.display = 'none';
 		}
-	});
+	}
+
+								// Sposób z pętlą forEach - zmienna list
+
+	// list.forEach((drink) => {
+	// 	if (drink.textContent.toLowerCase().indexOf(text) !== -1) {
+	// 		drink.style.display = 'block';
+	// 	} else {
+	// 		drink.style.display = 'none';
+	// 	}
+	// });
 };
 
 input.addEventListener('keyup', checkList);

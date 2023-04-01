d2d036b4555c5c22ac366eb9277fc2eddaeef3fe
Kamil Łaskarzewski <k4mlas@gmail.com>
2023-04-01 14:44:19 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const input = document.querySelector('.card__header__search');
+const list = document.querySelectorAll('li');
+
+const checkList = () => {
+	// input.value
+	console.log('Jest ok');
+};
+
+
+
+input.addEventListener('keyup', checkList);
